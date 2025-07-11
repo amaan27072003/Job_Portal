0ebe2ced756521f10c6957500d4570c81f9cc4a3
amaan27072003 <amaan27200307@gmail.com>
2025-07-11 22:22:39 +0530

    Added root URL for homepage

--- jobforme/urls.py
+++ jobforme/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
+
 
 urlpatterns = [
+    path('', lambda request: render(request, 'index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('', include('authuser.urls')),
     path('', include('candidate.urls')),
